3c81688cf033e1940ed2f3892e8c3fbbc1c9ec15
KevinAmani <15861800534@163.com>
2019-05-10 16:04:34 -0400

    new myreact https

--- myreact/src/MainPage/Config.js
+++ myreact/src/MainPage/Config.js
@@ -1,7 +1,8 @@
 
 //It is the link of resouces
 global.constants = {
-    website:"https://52.91.77.170:8083/img/",
+    //website:"https://52.91.77.170:8083/img/",
+    website:"https://www.chronoswood.com/img/",
     mainsite:"https://52.91.77.170:8083/",
     //website:"http://localhost:8083/img/",
     //mainsite:"http://localhost:8083/",
