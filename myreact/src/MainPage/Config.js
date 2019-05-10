578b48d5ee88ebe856ffcc76e0bdebd68f4f7bf8
KevinAmani <15861800534@163.com>
2019-05-10 16:17:38 -0400

    new build2

--- myreact/src/MainPage/Config.js
+++ myreact/src/MainPage/Config.js
@@ -3,7 +3,7 @@
 global.constants = {
     //website:"https://52.91.77.170:8083/img/",
     website:"https://www.chronoswood.com/img/",
-    mainsite:"https://52.91.77.170:8083/",
+    mainsite:"https://www.chronoswood.com/",
     //website:"http://localhost:8083/img/",
     //mainsite:"http://localhost:8083/",
     username:'',
